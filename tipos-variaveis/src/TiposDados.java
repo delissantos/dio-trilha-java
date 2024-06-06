2e615a4703758a3850f992c67c905ea9bf35be60
delissantos <lisgmsantos@gmail.com>
2024-06-05 21:26:31 -0300

    eee

---
+++ tipos-variaveis/src/TiposDados.java
@@ -0,0 +1,11 @@
+public class TiposDados {
+    
+    public static void main(String[] args) {
+        byte idade = 24;
+        short ano = 2024;
+        int cep = 21070333; // se começar com zero, não armazena no tipo int, provavalmente seja string. tambem vale pro long
+        long cpf = 12399488200L; 
+        float pi = 3.14F; // termina com F pra não confundir com double
+        double salario = 1243.33; 
+    }
+}
